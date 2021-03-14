9e465bc2ae313af97339ceab1385fb60ae00db37
Xavier Verges <460418+xverges@users.noreply.github.com>
2021-03-14 17:17:51 +0100

    Change logging order

--- remoter-client/src/init.r
+++ remoter-client/src/init.r
@@ -4,16 +4,15 @@ require("ids")
 # get request from the arguments of the docker run command, and escape all quotes.
 request <- commandArgs(trailingOnly = TRUE)[1]
 run_id <- ids::random_id()
+message("Got request with ID", run_id, "...")
+message(request)
+message("")
 
 # load wrapper in case it changed from last run
 message("Loading wrapper...")
 remoter::batch(addr = "host.docker.internal", port = 6969, file = "./wrapper.r")
 
-message("")
-message("Submitting request to pipeline server with ID", run_id, "...")
-message(request)
-message("")
-
+message('')
 message('Copying request...')
 message(sprintf("c2s(request, 'request_%s')", run_id))
 remoter::batch(addr = "host.docker.internal", port = 6969, script = sprintf("c2s(request, 'request_%s')", run_id))
