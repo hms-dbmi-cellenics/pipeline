3c2971551b6ab2911555d5f270a7d31c5158e01b
Xavier Verges <460418+xverges@users.noreply.github.com>
2021-03-16 16:19:11 +0100

    Debug remoter client

--- remoter-client/src/init.r
+++ remoter-client/src/init.r
@@ -12,13 +12,19 @@ parsed = RJSONIO::fromJSON(request)
 
 # load wrapper in case it changed from last run
 message("Loading wrapper for server ", parsed$server, "...")
+try(
 remoter::batch(addr = parsed$server, port = 6969, file = "./wrapper.r")
-
+)
 message('')
 message('Copying request...')
 message(sprintf("c2s(request, 'request_%s')", run_id))
+try(
 remoter::batch(addr = parsed$server, port = 6969, script = sprintf("c2s(request, 'request_%s')", run_id))
-
+)
 message('Launching work...')
 message(sprintf("wrapper(request_%s)", run_id))
+try(
 remoter::batch(addr = parsed$server, port = 6969, script = sprintf("wrapper(request_%s)", run_id))
+)
+message('About to sleep 60 seconds before exiting')
+Sys.sleep(60)
