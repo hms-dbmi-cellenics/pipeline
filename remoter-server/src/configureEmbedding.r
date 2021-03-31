6e4be6889ef92d4737d772941087429725dfeede
ogibson-biomage <76957896+ogibson-biomage@users.noreply.github.com>
2021-03-31 15:49:52 -0300

    Fix grey color on samples embedding (#27)
    * Fix grey color on samples embedding
    
    * Change
    
    * Fix additional bug

--- remoter-server/src/configureEmbedding.r
+++ remoter-server/src/configureEmbedding.r
@@ -205,7 +205,7 @@ coloring_samples_and_cluster <- function(scdata){
         color_pool <- scdata@misc[['color_pool']]
     }else{ # THIS SHOULD BE REMOVE ONCE THE EXPERIMENT HAS BEEN UPDATED WITH THE NEW VERSION OF THE DATA-INGEST 
         color_pool <- c("#e377c2","#8c564b","#d62728","#2ca02c","#ff7f0e","#1f77b4","#f8e71c","#3957ff","#d3fe14",
-        "#c9080a","#fec7f8","#0b7b3e","#0bf0e9","#c203c8","#fd9b39","#888593","#906407","#98ba7f","#fe6794","#10b0ff",
+        "#c9080a","#fec7f8","#0b7b3e","#0bf0e9","#c203c8","#fd9b39","#906407","#98ba7f","#fe6794","#10b0ff",
         "#ac7bff","#fee7c0","#964c63","#1da49c","#0ad811","#bbd9fd","#fe6cfe","#297192","#d1a09c","#78579e","#81ffad",
         "#739400","#ca6949","#d9bf01","#646a58","#d5097e","#bb73a9","#ccf6e9","#9cb4b6","#b6a7d4","#9e8c62","#6e83c8",
         "#01af64","#a71afd","#cfe589","#d4ccd1","#fd4109","#bf8f0e","#2f786e","#4ed1a5","#d8bb7d","#a54509","#6a9276",
