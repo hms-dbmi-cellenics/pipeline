c982ad70e1fb5f0274b9869734003b3df72852b9
Alex Pickering <alexvpickering@gmail.com>
2021-05-19 16:27:30 -0700

    reflect nan bugfix from data-inegest

--- pipeline-runner/src/data-ingest/help.r
+++ pipeline-runner/src/data-ingest/help.r
@@ -1,8 +1,8 @@
 
-# get_doublet_score function 
+# get_doublet_score function
 #' @description Get the cells with its doublet scores computed previously through scrublet
 #' @param sample name of the sample to retrieve the doublet scores
-#' 
+#'
 #' @export data.frame with barcodes and doublet scores
 
 get_doublet_score <- function(sample) {
@@ -12,18 +12,18 @@ get_doublet_score <- function(sample) {
         )
 
     colnames(scores) <- c("barcodes", "doublet_scores", "doublet_class")
-    rownames(scores) <- scores$barcodes    
+    rownames(scores) <- scores$barcodes
     return(as.data.frame(scores))
 }
 
 
 
-# check_config function 
+# check_config function
 #' @description Create metadata dataframe from config files
 #' @param scdata matrix with barcodes as columns to assign metadata information
 #' @param sample name of the sample to retrieve the metadata
 #' @param config config list from meta.json
-#' 
+#'
 #' @export dataframe with metadata information of the sample
 
 check_config <- function(scdata, sample, config){
@@ -34,11 +34,11 @@ check_config <- function(scdata, sample, config){
     # going to include in our experiment.
     if("metadata" %in% names(config)){
         rest_metadata <- as.data.frame(config$metadata)
-        rest_metadata$sample <- ifelse(length(config$samples)>1, config$samples, sample)
+        rest_metadata$sample <- config$samples
         for(var in colnames(rest_metadata)){
             metadata[, var] <- rest_metadata[, var][match(metadata$sample, rest_metadata$sample)]
         }
     }
-    
+
     return(metadata)
 }
