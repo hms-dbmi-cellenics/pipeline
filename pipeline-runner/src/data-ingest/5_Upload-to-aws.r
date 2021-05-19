aa3a681a57248e561d9f1a34e6cd595344704126
Alex Pickering <alexvpickering@gmail.com>
2021-05-19 16:26:49 -0700

    send dynamodb items to api

--- pipeline-runner/src/data-ingest/5_Upload-to-aws.r
+++ pipeline-runner/src/data-ingest/5_Upload-to-aws.r
@@ -1,48 +1,48 @@
-color_pool <- RJSONIO::fromJSON("data-ingest/color_pool.json")
+color_pool <- RJSONIO::fromJSON("/src/data-ingest/color_pool.json")
 
 # set to local dirs for interactive dev
 input_dir <- '/input'
 output_dir <- '/output'
 
 # creates the table information for samples
-create_samples_table <- function(config, experimend_id) {
+create_samples_table <- function(config, experiment_id) {
   # In samples_table we are going to add the core of the information
   samples_table <- list()
-  
+
   # flag_filtered information
   df_prefiltered <- read.csv(file.path(output_dir, 'df_flag_filtered.txt'),
                              sep = '\t',
                              row.names = 'samples')
-  
+
   samples <- row.names(df_prefiltered)
   samples_table$ids = paste0("sample-", samples)
-  
+
   # For the current datasets it could happen that they are not in the gz format, so we leave the alternative tsv format.
   mime_options = c(
     "tsv" = "application/tsv",
     "gz" = "application/gzip",
     "mtx" = "application/mtx"
   )
-  
+
   for (sample in samples) {
-    
+
     prefiltered <- df_prefiltered[sample, 'flag_filtered'] == 'Filtered'
-    
+
     # Identify datetime
     cdate <- mdate <- Sys.time()
     fnames <- list()
-    
+
     # files that are not hidden
     sample_files <- file.path(
       sample,
       list.files(file.path(input_dir, sample))
     )
-    
+
     # Iterate over each file to create the slot
     for (sample_file in sample_files) {
-      
+
       fext <- tail(strsplit(sample_file, '[.]')[[1]], 1)
-      
+
       fnames[[sample_file]] <- list(
         objectKey = '',
         name = sample_file,
@@ -52,7 +52,7 @@ create_samples_table <- function(config, experimend_id) {
         error = FALSE
       )
     }
-    
+
     # Add the whole information to each sample
     samples_table[[paste0("sample-", sample)]] <- list(
       name = sample,
@@ -69,66 +69,64 @@ create_samples_table <- function(config, experimend_id) {
     )
 
   }
-  
-  
+
+
   return(list(
     "experimentId" = experiment_id,
     "samples" = samples_table))
-  
+
 }
 
 
 samples_sets <- function(){
-  sample_annotations <- read.csv(file.path(output_dir, "samples-cells.csv"), 
+  sample_annotations <- read.csv(file.path(output_dir, "samples-cells.csv"),
                                  sep = "\t",
                                  col.names = c("Cells_ID","Value"),
                                  na.strings = "None")
-  
-  cell_set <- list(key = "sample", 
-                  name = "Samples",
-                  rootNode = TRUE,
-                  children = list(),
-                  type = "metadataCategorical")
-  
+
+  cell_set <- list(key = "sample",
+                   name = "Samples",
+                   rootNode = TRUE,
+                   children = list(),
+                   type = "metadataCategorical")
+
   samples <- unique(sample_annotations$Value)
-  
+
   for (sample in samples) {
     view <- sample_annotations[sample_annotations$Value == sample, "Cells_ID"]
-    child <- list(key = paste0("sample-", sample), 
+    child <- list(key = paste0("sample-", sample),
                   name = sample,
                   color = color_pool[1],
                   cellIds = view)
-    
+
     color_pool <- color_pool[-1]
-    cell_set$children <- append(cell_set$children, child)  
+    cell_set$children <- append(cell_set$children, child)
   }
-  
+
   return(cell_set)
 }
 
 # cell_sets fn for seurat metadata information
 meta_sets <- function() {
-  
+
   meta_annotations <- read.csv(file.path(output_dir, "metadata-cells.csv"), sep='\t')
-  
+
   cell_set_list <- list()
-  
+
   # The first column is the cells_id, the rest is the metadata information
   for (i in seq(2, ncol(meta_annotations))) {
     key <- name <- colnames(meta_annotations)[i]
-    
+
     cell_set = list(
       "key" = key,
       "name" = name,
       "rootNode" = TRUE,
       "children" = list(),
       "type" = "metadataCategorical"
-      
-      
     )
-    
+
     annot <- meta_annotations[[i]]
-    
+
     for (value in unique(annot)) {
       view  <- meta_annotations[which(annot == value), 'cells_id']
       cell_set$children <- append(
@@ -139,7 +137,7 @@ meta_sets <- function() {
           "color" = color_pool[1],
           "cellIds" = view)
       )
-      
+
       color_pool <- color_pool[-1]
     }
     cell_set_list <- append(cell_set_list, cell_set)
@@ -149,25 +147,21 @@ meta_sets <- function() {
 
 
 task <- function(input, pipeline_config) {
-  
-  
-  
-  # is this right?
+
   experiment_id <- input$experimentId
-  
   project_id <- input$projectId
-  sample_names <- input$sampleNames # extract sample names from samples object
+  sample_names <- input$sampleNames
   sample_uuids <- input$sampleUuids
 
   # save experiment_id for record-keeping
   writeLines(experiment_id, file.path(output_dir, "experiment_id.txt"))
-  
-  
+
+  # read experiment config
   config <- RJSONIO::fromJSON(file.path(input_dir, "meta.json"))
-  
-  # read config related with QC pipeline
+
+  # read config related to QC pipeline
   config_dataProcessing <- RJSONIO::fromJSON(file.path(output_dir, "config_dataProcessing.json"))
-  
+
   # Design cell_set scratchpad for DynamoDB
   scratchpad <- list(
     key = "scratchpad",
@@ -176,20 +170,20 @@ task <- function(input, pipeline_config) {
     children = list(),
     type = "cellSets"
   )
-  
+
   # TODO: maybe we don't need samples_data
   samples_data <- create_samples_table(config, experiment_id)
   samples_set <- samples_sets()
-  
+
   # Design cell_set meta_data for DynamoDB
   cell_sets <- list(scratchpad = scratchpad, samples_set = samples_set)
-  
+
   if ("metadata" %in% names(config))
     cell_sets$meta_sets <- meta_sets()
-  
+
   print(paste("Experiment name is", config$name))
-  
-  
+
+
   experiment_data <- list(
     apiVersion = "2.0.0-data-ingest-seurat-rds-automated",
     experimentId = experiment_id,
@@ -200,38 +194,36 @@ task <- function(input, pipeline_config) {
     ),
     processingConfig = config_dataProcessing
   )
-  
-  # experiment data to dynamodb
-  put_item_in_dynamodb(pipeline_config,
-                       table = pipeline_config$experiments_table,
-                       item = experiment_data)
-  
+
+
+  send_dynamodb_item_to_api(pipeline_config,
+                            table = pipeline_config$experiments_table,
+                            item = experiment_data)
+
   # samples data to dynamodb
-  put_item_in_dynamodb(pipeline_config,
-                       table = pipeline_config$samples_table,
-                       item = samples_data)
-  
-  
+  send_dynamodb_item_to_api(pipeline_config,
+                            table = pipeline_config$samples_table,
+                            item = samples_data)
+
+
   # cell sets file to s3
-  cell_sets_file <- tempfile()
   cell_sets_data <- RJSONIO::toJSON(cell_sets)
-  write(cell_sets_data, cell_sets_file)
-  
-  put_file_in_s3(pipeline_config,
-                 bucket = pipeline_config$cell_sets_bucket,
-                 file = cell_sets_file,
-                 key = experiment_id)
-  
+
+  put_object_in_s3(pipeline_config,
+                   bucket = pipeline_config$cell_sets_bucket,
+                   object = charToRaw(cell_sets_data),
+                   key = experiment_id)
+
   # seurat object to s3
-  put_file_in_s3(pipeline_config,
-                 bucket = pipeline_config$source_bucket,
-                 file = file.path(output_dir, 'experiment.rds'),
-                 key = file.path(experiment_id, 'r.rds'))
-  
+  put_object_in_s3(pipeline_config,
+                   bucket = pipeline_config$source_bucket,
+                   object = file.path(output_dir, 'experiment.rds'),
+                   key = file.path(experiment_id, 'r.rds'))
+
   cluster_env <- pipeline_config$cluster_env
   print(sprintf("Experiment ID: %s uploaded to %s.", experiment_id, cluster_env))
-  
-  if (cluster_env == "production") 
+
+  if (cluster_env == "production")
     print(sprintf("https://scp.biomage.net/experiments/%s/data-exploration", experiment_id))
 
 }
--- pipeline-runner/src/handle_data.r
+++ pipeline-runner/src/handle_data.r
@@ -7,7 +7,7 @@ reload_scdata_from_s3 <- function(pipeline_config, experiment_id) {
     s3 <- paws::s3(config=pipeline_config$aws_config)
     message(pipeline_config$source_bucket)
     message(paste(experiment_id, "r.rds", sep = "/"))
-    
+
     c(body, ...rest) %<-% s3$get_object(
         Bucket = pipeline_config$source_bucket,
         Key = paste(experiment_id, "r.rds", sep = "/")
@@ -18,7 +18,7 @@ reload_scdata_from_s3 <- function(pipeline_config, experiment_id) {
 
 send_output_to_api <- function(pipeline_config, input, plot_data_keys, output) {
     c(config, plot_data = plotData) %<-% output
-    
+
     # upload output
     s3 <- paws::s3(config=pipeline_config$aws_config)
     id <- ids::uuid()
@@ -29,17 +29,17 @@ send_output_to_api <- function(pipeline_config, input, plot_data_keys, output) {
             plotData = plot_data
         )
     )
-    
+
     message("Uploading results to S3 bucket", pipeline_config$results_bucket, " at key ", id, "...")
     s3$put_object(
         Bucket = pipeline_config$results_bucket,
         Key = id,
         Body = charToRaw(output)
     )
-    
+
     message("Sending to SNS topic ", pipeline_config$sns_topic)
     sns <- paws::sns(config=pipeline_config$aws_config)
-    
+
     msg <- list(
         input = input,
         output = list(
@@ -50,7 +50,7 @@ send_output_to_api <- function(pipeline_config, input, plot_data_keys, output) {
             error = FALSE
         )
     )
-    
+
     result <- sns$publish(
         Message = RJSONIO::toJSON(msg),
         TopicArn = pipeline_config$sns_topic,
@@ -62,30 +62,30 @@ send_output_to_api <- function(pipeline_config, input, plot_data_keys, output) {
             )
         )
     )
-    
+
     return(result$MessageId)
 }
 
-send_plot_data_to_s3 <- function(pipeline_config, experiment_id, output) { 
+send_plot_data_to_s3 <- function(pipeline_config, experiment_id, output) {
     plot_data <- output$plotData
-    
+
     s3 <- paws::s3(config=pipeline_config$aws_config)
-    
+
     plot_names <- names(plot_data)
-    
+
     plot_data_keys = list()
-    
+
     for(plot_data_name in names(plot_data)){
         id <- ids::uuid()
-        
+
         plot_data_keys[[plot_data_name]] <- id
-        
+
         output <- RJSONIO::toJSON(
             list(
                 plotData = plot_data[[plot_data_name]]
             )
         )
-        
+
         message("Uploading plotData to S3 bucket", pipeline_config$plot_data_bucket, " at key ", id, "...")
         s3$put_object(
             Bucket = pipeline_config$plot_data_bucket,
@@ -93,44 +93,63 @@ send_plot_data_to_s3 <- function(pipeline_config, experiment_id, output) {
             Body = charToRaw(output)
         )
     }
-    
+
     return(plot_data_keys)
 }
 
 upload_matrix_to_s3 <- function(pipeline_config, experiment_id, data) {
-    
+
     s3 <- paws::s3(config=pipeline_config$aws_config)
-    
+
     object_key <- paste0(experiment_id, '/r.rds')
-    
+
     count_matrix <- tempfile()
     saveRDS(data, file=count_matrix)
-    
+
     message("Uploading updated count matrix to S3 bucket ", pipeline_config$processed_bucket, " at key ", object_key, "...")
     s3$put_object(
         Bucket = pipeline_config$processed_bucket,
         Key = object_key,
         Body = count_matrix
     )
-    
+
     return(object_key)
 }
 
 
-put_item_in_dynamodb <- function(pipeline_config, table, item) {
-    
-    dynamodb <- paws::dynamodb(config=pipeline_config$aws_config)
-    dynamodb$put_item(
-        TableName = table,
-        Item = item)
+send_dynamodb_item_to_api <- function(pipeline_config, table, item) {
+
+    message("Sending to SNS topic ", pipeline_config$sns_topic)
+    sns <- paws::sns(config=pipeline_config$aws_config)
+
+    msg <- list(
+        item = item,
+        table = table)
+
+    result <- sns$publish(
+        Message = RJSONIO::toJSON(msg),
+        TopicArn = pipeline_config$sns_topic,
+        MessageAttributes = list(
+            type = list(
+                DataType = "String",
+                StringValue = "PipelineResponse",
+                BinaryValue = NULL
+            )
+        )
+    )
+
+    return(result$MessageId)
 }
 
-put_file_in_s3 <- function(pipeline_config, bucket, file, key) {
-    
+put_object_in_s3 <- function(pipeline_config, bucket, object, key) {
+
+    print(sprintf("Putting %s in %s", key, bucket))
+
     s3 <- paws::s3(config=pipeline_config$aws_config)
     s3$put_object(
         Bucket = bucket,
         Key = key,
-        Body = file
+        Body = object
     )
 }
+
