2941b52a380d0c365ea76dcb1514ae311cf70efc
Anugerah Erlaut <aerlaut@live.com>
2021-05-29 10:33:42 +0700

    fix data structures

--- pipeline-runner/src/data-ingest/0-download_gem2s.r
+++ pipeline-runner/src/data-ingest/0-download_gem2s.r
@@ -52,6 +52,7 @@ task <- function(input, pipeline_config) {
     #writeBin(body, con = "/input/meta.json")
     config <- list(name = input$experimentName, samples=input$sampleIds,
     organism = input$organism,
+    metadata = input$metadata,
     input = list(type="10x")
     )
 
