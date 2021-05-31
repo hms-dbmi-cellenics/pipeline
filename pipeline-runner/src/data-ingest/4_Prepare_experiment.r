e89206afba3b9f96024c7873f96d306c22578c85
Juanlu <76958039+JuanluOnieva-biomage@users.noreply.github.com>
2021-05-31 18:35:50 +0200

    Fix fast mnn (#75)
    * Sample ids property sent as list instead of string
    
    * Add FastMNN
    
    * Restore 5_Upload-to-aws

--- pipeline-runner/src/data-ingest/4_Prepare_experiment.r
+++ pipeline-runner/src/data-ingest/4_Prepare_experiment.r
@@ -350,7 +350,8 @@ task <- function(input,pipeline_config){
     config.dataIntegration <- list(auto="false", 
         dataIntegration = list( method = identified.method , 
                             methodSettings = list(seuratv4=list(numGenes=2000, normalisation="logNormalize"), 
-                                                unisample=list(numGenes=2000, normalisation="logNormalize"))),
+                                                unisample=list(numGenes=2000, normalisation="logNormalize"),
+                                                fastmnn=list(numGenes=2000, normalisation="logNormalize"))),
         dimensionalityReduction = list(method = "rpca", numPCs = 30, excludeGeneCategories = c())
     )
 
