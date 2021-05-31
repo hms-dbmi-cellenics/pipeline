6cb5f74c874e93c0140c2ef74dc1036a5dd4fe4d
Juanlu <juanlu@biomage.net>
2021-05-31 17:21:57 +0200

    Restore 5_Upload-to-aws

--- pipeline-runner/src/data-ingest/5_Upload-to-aws.r
+++ pipeline-runner/src/data-ingest/5_Upload-to-aws.r
@@ -15,7 +15,7 @@ create_samples_table <- function(config, experiment_id, project_id) {
                              row.names = 'samples')
 
   samples <- row.names(df_prefiltered)
-  samples_table$ids = list(paste0("sample-", samples))
+  samples_table$ids = paste0("sample-", samples)
 
   # For the current datasets it could happen that they are not in the gz format, so we leave the alternative tsv format.
   mime_options = c(
