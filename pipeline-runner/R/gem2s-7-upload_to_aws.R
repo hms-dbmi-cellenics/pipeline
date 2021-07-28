07e30b04dd84087d16d019d9245e2e91d9370c9b
Alex Pickering <alexvpickering@gmail.com>
2021-07-28 14:30:19 -0700

    init refactor upload_to_aws

--- pipeline-runner/R/gem2s-7-upload_to_aws.R
+++ pipeline-runner/R/gem2s-7-upload_to_aws.R
@@ -1,59 +1,20 @@
-upload_to_aws <- function(input, pipeline_config) {
-  # set to local dirs for interactive dev
-  input_dir <- "/input"
-  output_dir <- "/output"
+
+upload_to_aws <- function(input, pipeline_config, prev_out) {
+  message('Uploading to AWS ...')
 
   experiment_id <- input$experimentId
   project_id <- input$projectId
 
-  # save experiment_id for record-keeping
-  writeLines(experiment_id, file.path(output_dir, "experiment_id.txt"))
+  # destructure what need from prev_out
+  scdata <- prev_out$scdata
+  config <- prev_out$config
+  config_dataProcessing <- prev_out$qc_config
 
-  # read experiment config
-  config <- RJSONIO::fromJSON(file.path(input_dir, "/meta.json"))
+  saveRDS(input, '/debug/input.rds')
+  saveRDS(config, '/debug/config.rds')
 
-  print_config(7, "Upload to AWS", input, pipeline_config, config)
-
-  # read config related to QC pipeline
-  config_dataProcessing <- RJSONIO::fromJSON(file.path(output_dir, "config_dataProcessing.json"))
-
-  # Design cell_set scratchpad for DynamoDB
-  scratchpad <- list(
-    key = "scratchpad",
-    name = "Scratchpad",
-    rootNode = TRUE,
-    children = list(),
-    type = "cellSets"
-  )
-
-  color_pool <- get_color_pool()
-  samples_set <- samples_sets(input, output_dir, color_pool)
-
-  # remove used colors from pool
-  used <- seq_along(samples_set$children)
-  color_pool <- color_pool[-used]
-
-  # Design cell_set meta_data for DynamoDB
-  cell_sets <- c(list(scratchpad), list(samples_set))
-
-  if ("metadata" %in% names(config)) {
-    cell_sets <- c(cell_sets, meta_sets(output_dir, color_pool))
-  }
-
-  cell_sets <- list(cellSets = cell_sets)
-
-  print(paste("Experiment name is", config$name))
-
-  experiment_data <- list(
-    apiVersion = "2.0.0-data-ingest-seurat-rds-automated",
-    experimentId = experiment_id,
-    experimentName = config$name,
-    meta = list(
-      organism = config$organism,
-      type = config$input[["type"]]
-    ),
-    processingConfig = config_dataProcessing
-  )
+  message("\tConstructing cell sets ...")
+  cell_sets <- get_cell_sets(scdata, input)
 
   # cell sets file to s3
   cell_sets_data <- RJSONIO::toJSON(cell_sets)
@@ -65,33 +26,58 @@ upload_to_aws <- function(input, pipeline_config) {
   )
 
   # seurat object to s3
+  message("\tUploading Seurat Object to S3 ...")
+  fpath <- file.path(tempdir(), 'experiment.rds')
+  saveRDS(scdata, fpath)
+
   put_object_in_s3_multipart(pipeline_config,
     bucket = pipeline_config$source_bucket,
-    object = file.path(output_dir, "experiment.rds"),
+    object = fpath,
     key = file.path(experiment_id, "r.rds")
   )
 
   cluster_env <- pipeline_config$cluster_env
-  print(sprintf("Experiment ID: %s uploaded to %s.", experiment_id, cluster_env))
 
   data <- list(
     item = experiment_data,
     table = pipeline_config$experiments_table
   )
 
-  if (cluster_env == "production") {
-    print(sprintf("https://scp.biomage.net/experiments/%s/data-exploration", experiment_id))
-  }
-
   return(data)
 }
 
-samples_sets <- function(config, output_dir, color_pool) {
-  sample_annotations <- read.csv(file.path(output_dir, "samples-cells.csv"),
-    sep = "\t",
-    col.names = c("Cells_ID", "Value"),
-    na.strings = "None"
+# creates initial cell sets object
+get_cell_sets <- function(scdata, input) {
+
+  scratchpad <- list(
+    key = "scratchpad",
+    name = "Scratchpad",
+    rootNode = TRUE,
+    children = list(),
+    type = "cellSets"
   )
+
+  color_pool <- get_color_pool()
+  samples_set <- samples_sets(input, scdata, color_pool)
+
+  # remove used colors from pool
+  used <- seq_along(samples_set$children)
+  color_pool <- color_pool[-used]
+
+  # Design cell_set meta_data for DynamoDB
+  cell_sets <- c(list(scratchpad), list(samples_set))
+
+  # TODO: refactor meta_sets
+  if ("metadata" %in% names(config)) {
+    cell_sets <- c(cell_sets, meta_sets(output_dir, color_pool))
+  }
+
+  cell_sets <- list(cellSets = cell_sets)
+}
+
+# cell_sets fn for seurat samples information
+samples_sets <- function(input, scdata, color_pool) {
+
 
   cell_set <- list(
     key = "sample",
@@ -101,17 +87,18 @@ samples_sets <- function(config, output_dir, color_pool) {
     type = "metadataCategorical"
   )
 
-  samples <- unique(sample_annotations$Value)
+  cell_sample <- scdata$samples
+  samples <- unique(cell_sample)
 
   for (i in seq_along(samples)) {
-    sample <- toString(samples[i])
-    view <- sample_annotations[sample_annotations$Value == sample, "Cells_ID"]
+    sample <- samples[i]
+    cell_ids <- scdata$cells_id[cell_sample == sample]
 
     cell_set$children[[i]] <- list(
       key = paste0(sample),
-      name = toString(config$sampleNames[[match(sample, config$sampleIds)]]),
+      name = toString(input$sampleNames[[match(sample, input$sampleIds)]]),
       color = color_pool[i],
-      cellIds = view
+      cellIds = cell_ids
     )
   }
 
