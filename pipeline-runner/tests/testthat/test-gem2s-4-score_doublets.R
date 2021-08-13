4ea7584914e662f4e168b1c007c62be919400e2f
Alex Pickering <alexvpickering@gmail.com>
2021-08-13 11:28:32 -0700

    test that score_doublets filters cells to avoid low counts

--- pipeline-runner/tests/testthat/test-gem2s-4-score_doublets.R
+++ pipeline-runner/tests/testthat/test-gem2s-4-score_doublets.R
@@ -14,3 +14,14 @@ test_that("score_doublets returns expected columns", {
     expect_setequal(colnames(out$doublet_scores$sample1),
                     c('barcodes', 'doublet_class', 'doublet_scores'))
 })
+
+
+test_that("score_doublets filters cells to avoid warning of extremely low read counts", {
+    counts <- mock_counts(ncells = c(200, 300, 400, 200, 500, 300))
+    counts <- round(counts/2)
+
+    expect_warning(scDblFinder:::.checkSCE(counts), 'extremely low read counts')
+
+    prev_out <- list(counts_list = list(sample1=counts))
+    expect_warning(score_doublets(NULL, NULL, prev_out)$output, NA)
+})
