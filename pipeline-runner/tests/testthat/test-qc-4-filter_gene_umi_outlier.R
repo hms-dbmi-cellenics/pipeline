12e2c9f86e2b6d05ccb16bc0ba2593ba6239419f
Alex Pickering <alexvpickering@gmail.com>
2021-10-12 16:58:27 -0700

    add spline fit test

--- pipeline-runner/tests/testthat/test-qc-4-filter_gene_umi_outlier.R
+++ pipeline-runner/tests/testthat/test-qc-4-filter_gene_umi_outlier.R
@@ -141,7 +141,21 @@ test_that("filter_gene_umi_outlier skips if no barcodes for this sample", {
 })
 
 
-test_that("filter_gene_umi_outlier works with spline fit", {
-    message('TODO')
+test_that("filter_gene_umi_outlier gives different results with spline fit", {
+    # single outlier in single sample
+    scdata <- mock_scdata(with_outlier = TRUE)
+    config <- mock_config()
+    nstart <- ncol(scdata)
+    scdata$samples <- '123abc'
+
+    out1 <- filter_gene_umi_outlier(scdata, config, '123abc')
+    expect_lt(ncol(out1$data), nstart)
+
+    config$filterSettings$regressionType <- 'spline'
+
+    out2 <- filter_gene_umi_outlier(scdata, config, '123abc')
+
+    expect_true(!identical(out1$plotData, out2$plotData))
+    expect_true(!identical(out1$data, out2$data))
 })
 
