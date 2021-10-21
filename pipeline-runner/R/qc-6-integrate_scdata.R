de8bbc1abe818d7825957e33e48b7574255c7c36
Oliver Gibson <olivergibson@MacBook-Pro-de-Oliver.local>
2021-10-21 00:03:29 -0300

    Fix dep

--- pipeline-runner/R/qc-6-integrate_scdata.R
+++ pipeline-runner/R/qc-6-integrate_scdata.R
@@ -204,7 +204,7 @@ run_fastmnn <- function(scdata, config) {
 
   # @misc slots not preserved so transfer
   misc <- scdata@misc
-  scdata <- SeuratWrappers::RunFastMNN(object.list = SplitObject(scdata, split.by = "samples"), d = 50, get.variance = TRUE)
+  scdata <- SeuratWrappers::RunFastMNN(object.list = Seurat::SplitObject(scdata, split.by = "samples"), d = 50, get.variance = TRUE)
   scdata@misc <- misc
   scdata@misc[["active.reduction"]] <- "mnn"
 
--- pipeline-runner/tests/testthat/test-qc-6-integrate_scdata.R
+++ pipeline-runner/tests/testthat/test-qc-6-integrate_scdata.R
@@ -22,3 +22,35 @@ test_that("harmony integration works", {
   scdata <- suppressWarnings(run_dataIntegration(scdata, config))
   expect_s4_class(scdata, "Seurat")
 })
+
+test_that("SeuratV4 integration doesnt error out with small dataset", {
+  scdata <- mock_scdata()
+  config <- list(
+    dimensionalityReduction = list(numPCs = 2),
+    dataIntegration = list(method = "seuratv4", methodSettings = list(seuratv4 = list(numGenes = 1000, normalisation = "logNormalize")))
+  )
+
+  scdata <- suppressWarnings(run_dataIntegration(scdata, config))
+  expect_s4_class(scdata, "Seurat")
+})
+
+test_that("Unisample integration works", {
+  scdata <- mock_scdata()
+  config <- list(
+    dimensionalityReduction = list(numPCs = 2),
+    dataIntegration = list(method = "unisample", methodSettings = list(unisample = list(numGenes = 1000, normalisation = "logNormalize")))
+  )
+
+  scdata <- suppressWarnings(run_dataIntegration(scdata, config))
+  expect_s4_class(scdata, "Seurat")
+})
+
+test_that("FastMNN is not working", {
+  scdata <- mock_scdata()
+  config <- list(
+    dimensionalityReduction = list(numPCs = 2),
+    dataIntegration = list(method = "fastmnn", methodSettings = list(fastmnn = list(numGenes = 1000, normalisation = "logNormalize")))
+  )
+
+  expect_error(suppressWarnings(run_dataIntegration(scdata, config)))
+})
