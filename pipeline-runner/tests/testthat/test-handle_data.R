8f6a90e0ac30acf85bfe4a4b85f52cc8c3b42fc8
alexvpickering <alexvpickering@gmail.com>
2021-10-27 12:52:01 +0100

    init mockery stub

---
+++ pipeline-runner/tests/testthat/test-handle_data.R
@@ -0,0 +1,27 @@
+
+mock_sns <- function(config) {
+    print('hello mock!')
+    return(
+        list(publish = function (Message, TopicArn, MessageAttributes) {
+            return (list(MessageId = 'ok'))
+        }
+    ))
+}
+
+test_that("send_gem2s_update_to_api completes successfully", {
+    pipeline_config <- list(
+        sns_topic = 'ExampleTopic',
+        aws_config = NULL
+    )
+
+    mockery::stub(send_gem2s_update_to_api, 'paws::sns', mock_sns)
+
+    response <- send_gem2s_update_to_api(pipeline_config,
+                            experiment_id = 'dfgdfg',
+                            task_name = 'dsfdsdf',
+                            data = 1:5,
+                            input = list(auth_JWT='ayylmao'))
+
+
+    expect_true(response == 'ok')
+})
