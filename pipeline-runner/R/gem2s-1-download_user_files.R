50ef20f7e2b4736eee8d6593453300d71870f08d
german <gerbeldo@gmail.com>
2022-02-24 16:40:48 -0300

    del hardcodes, simplify dir creation
    fs::dir_create creates intermidiate directories if they do not exist.
    Also, does not throw a warning if creating an existing directory.
    Having a single function call simplifies mocking.

--- pipeline-runner/R/gem2s-1-download_user_files.R
+++ pipeline-runner/R/gem2s-1-download_user_files.R
@@ -14,7 +14,9 @@ download_user_files <- function(input, pipeline_config, prev_out = list()) {
 
   s3 <- paws::s3(config = pipeline_config$aws_config)
 
-  unlink("/input", recursive = TRUE)
+  input_dir <- "/input"
+
+  unlink(input_dir, recursive = TRUE)
 
   for (sample in sample_uuids) {
     message("\nSample --> ", sample)
@@ -30,12 +32,10 @@ download_user_files <- function(input, pipeline_config, prev_out = list()) {
       message("GEM key: ", gem_key)
       fname <- basename(gem_key)
 
-      sample_name <- sample_names[[match(sample, sample_uuids)]]
       # Preparing directories
-      local_dir <- file.path("/input", sample)
-      dir.create("/input")
-      dir.create(local_dir)
+      local_dir <- file.path(input_dir, sample)
       local_fpath <- file.path(local_dir, fname)
+      fs::dir_create(local_dir)
 
       # Download the file and store the output in a variable
       c(body, ...rest) %<-% s3$get_object(
