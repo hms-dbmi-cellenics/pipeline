62cc10f7f036b5b6455a6a998119b9c126bd0556
german <gerbeldo@gmail.com>
2022-04-12 14:30:42 -0300

    clean

--- pipeline-runner/tests/testthat/test-qc-7-embed_and_cluster.R
+++ pipeline-runner/tests/testthat/test-qc-7-embed_and_cluster.R
@@ -1,5 +1,3 @@
-test_that("update_sets_through_api builds a correct request", {})
-
 with_fake_http(
   test_that("update_sets_through_api sends patch request", {
     expect_PATCH(update_sets_through_api(list(), "api_url", "experiment_id", "cell_set_key", "auth"))
