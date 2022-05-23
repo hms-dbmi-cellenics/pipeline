cc58959cb3644c1e5bc3cd11fec044845f1e4535
cosa65 <martin@biomage.net>
2022-05-23 14:17:40 +0200

    Change sampleS3Paths, now it is being sent as an object with sampleId as keys

--- pipeline-runner/R/gem2s-1-download_user_files.R
+++ pipeline-runner/R/gem2s-1-download_user_files.R
@@ -14,8 +14,7 @@ download_and_store <- function(bucket, key, file_path, s3) {
 
 #' Download user files from S3
 #'
-#' @param project_id
-#' @param sample_ids
+#' @param input The input params received from the api
 #' @param originals_bucket The bucket where the sample files are
 #' @param input_dir A string, where the s3 object is going to be stored
 #' @param s3 S3 aws client
@@ -52,10 +51,7 @@ get_gem2s_file_v1 <- function(input, originals_bucket, input_dir, s3) {
 
 #' Download user files from S3
 #'
-#' @param project_id
-#' @param sample_ids
-#' @param s3_paths_by_sample The key for each sample file stored in s3
-#' @param technology The technology used in the samples
+#' @param input The input params received from the api
 #' @param originals_bucket The bucket where the sample files are
 #' @param aws_config The input object from the request
 #' @param input_dir A string, where the s3 object is going to be stored
@@ -66,17 +62,14 @@ get_gem2s_file_v1 <- function(input, originals_bucket, input_dir, s3) {
 get_gem2s_file_v2 <- function(input, originals_bucket, input_dir, s3) {
   project_id <- input$projectId
   sample_ids <- input$sampleIds
-  s3_paths_by_sample <- input$sampleS3Paths
+  sample_s3_paths <- input$sampleS3Paths
   technology <- input$input$type
 
   unlink(input_dir, recursive = TRUE)
 
-  for (i in seq_along(sample_ids)) {
-    sample_id <- sample_ids[i]
-    sample_s3_paths <- s3_paths_by_sample[[i]]
-
+  for (sample_id in sample_ids) {
     for (file_type in file_types_by_technology[[technology]]) {
-      s3_path <- sample_s3_paths[[file_type]]
+      s3_path <- sample_s3_paths[[sample_id]][[file_type]]
 
       local_fpath <- file.path(input_dir, sample_id, file_names_v1[[file_type]])
       download_and_store(originals_bucket, s3_path, local_fpath, s3)
@@ -100,6 +93,8 @@ download_user_files <- function(input, pipeline_config, prev_out = list(), input
   if (input$apiVersion == "v1") {
     get_gem2s_file_v1(input, pipeline_config$originals_bucket, input_dir, s3)
   } else if (input$apiVersion == "v2") {
+    print("inputDebug")
+    print(input)
     get_gem2s_file_v2(input, pipeline_config$originals_bucket, input_dir, s3)
   }
 
