fe96f2e733b9f881d1ddf725a07eee8dbf63717f
Oliver Gibson <76957896+ogibson@users.noreply.github.com>
2022-06-01 12:03:02 +0200

    Update pipeline-runner/tests/testthat.R

--- pipeline-runner/tests/testthat.R
+++ pipeline-runner/tests/testthat.R
@@ -1,7 +1,9 @@
 library(testthat)
 library(pipeline)
 print(getwd())
-
+print(list.files("./",recursive=TRUE))
+print(list.files("../",recursive=TRUE))
+print(list.files("/src",recursive=TRUE))
 load('../pipeline-runner/R/sysdata.rda')
 
 test_check("pipeline")
