82ef64542cc2b0d11fb650fac1264a0425f248d1
Oliver Gibson <oliver@biomage.net>
2022-06-01 12:14:54 +0200

    fix

--- pipeline-runner/tests/testthat.R
+++ pipeline-runner/tests/testthat.R
@@ -1,9 +1,5 @@
 library(testthat)
 library(pipeline)
-print(getwd())
-print(list.files("./",recursive=TRUE))
-print(list.files("../",recursive=TRUE))
-print(list.files("/src",recursive=TRUE))
-load('../pipeline-runner/R/sysdata.rda')
+gem2s <- list(random.seed=42)
 
 test_check("pipeline")
