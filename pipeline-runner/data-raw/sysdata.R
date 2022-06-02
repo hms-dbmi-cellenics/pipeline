d201625120a10c1e5b83c134797c5ffc33e49919
German Beldorati Stark <gerbeldo@gmail.com>
2022-06-02 16:23:55 +0200

    add cell cycle gene lists for mice and human

--- pipeline-runner/data-raw/sysdata.R
+++ pipeline-runner/data-raw/sysdata.R
@@ -9,7 +9,7 @@ gem2s <- list(
 # mounted as a volume outside container to local-runner/debug
 DEBUG_PATH <- "/debug"
 
-# File management, it needs to match the sample_file_type enum in sql 
+# File management, it needs to match the sample_file_type enum in sql
 # (they are originally defined in 20220304184711_schema.js in the api)
 file_types_by_technology <- list(
   "10x" = list("barcodes10x", "features10x", "matrix10x")
@@ -21,11 +21,17 @@ file_names_v1 <- list(
   matrix10x = "matrix.mtx.gz"
 )
 
+source("data-raw/cell_cycle_genes.R")
+
+cc_genes <- list("human" = human_cc_genes,
+                 "mouse" = mouse_cc_genes)
+
 usethis::use_data(
-  gem2s, 
-  DEBUG_PATH, 
+  gem2s,
+  DEBUG_PATH,
   file_names_v1,
-  file_types_by_technology, 
-  internal = TRUE, 
+  file_types_by_technology,
+  cc_genes,
+  internal = TRUE,
   overwrite = TRUE
 )
