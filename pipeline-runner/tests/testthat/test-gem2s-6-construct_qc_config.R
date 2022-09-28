0da27f424e8153218779621ffadc51ab477f580b
stefanbabukov <stefanbabukov98@gmail.com>
2022-09-28 12:58:22 +0100

    test fix

--- pipeline-runner/tests/testthat/test-gem2s-6-construct_qc_config.R
+++ pipeline-runner/tests/testthat/test-gem2s-6-construct_qc_config.R
@@ -1,11 +1,10 @@
-mock_scdata <- function() {
+mock_scdata_list <- function() {
   pbmc_raw <- read.table(
     file = system.file("extdata", "pbmc_raw.txt", package = "Seurat"),
     as.is = TRUE
   )
 
   scdata <- Seurat::CreateSeuratObject(counts = pbmc_raw)
-
   # add samples
   scdata$samples <- rep("123abc", 80)
   scdata <- Seurat::RenameCells(scdata, paste(scdata$samples, colnames(scdata), sep = ""))
@@ -16,16 +15,20 @@ mock_scdata <- function() {
 
   # add mitochondrial percent
   scdata$percent.mt <- rnorm(ncol(scdata), mean = 6)
-  return(scdata)
+
+  # create an scdata_list with duplicated samples
+  scdata_list <- list()
+  for (sample_id in scdata$samples) {
+      scdata_list[[sample_id]] <- scdata
+  }
+  return(scdata_list)
 }
 
-
 test_that("cellsize filter is disabled by default and classifier is pre-filtered", {
-  scdata <- mock_scdata()
-  samples <- names(list(scdata))
-  qc_config <- construct_qc_config(list(scdata), any_filtered = TRUE)
+  scdata_list <- mock_scdata_list()
+  qc_config <- construct_qc_config(scdata_list, any_filtered = TRUE)
 
-  for (sample in unique(samples)) {
+  for (sample in names(scdata_list)) {
     expect_false(qc_config$classifier[[sample]]$enabled)
     expect_true(qc_config$classifier[[sample]]$prefiltered)
     expect_false(qc_config$cellSizeDistribution[[sample]]$enabled)
@@ -34,10 +37,10 @@ test_that("cellsize filter is disabled by default and classifier is pre-filtered
 
 
 test_that("cellsize filter is disabled by default and classifier is not pre-filtered", {
-  scdata <- mock_scdata()
-  samples <- names(list(scdata))
-  qc_config <- construct_qc_config(list(scdata), any_filtered = FALSE)
-  for (sample in unique(samples)) {
+  scdata_list <- mock_scdata_list()
+  qc_config <- construct_qc_config(scdata_list, any_filtered = FALSE)
+
+  for (sample in names(scdata_list)) {
     expect_false(qc_config$cellSizeDistribution[[sample]]$enabled)
     expect_true(qc_config$classifier[[sample]]$enabled)
     expect_false(qc_config$classifier[[sample]]$prefiltered)
