7bfd952fe0837167c8925a5e1c5cd34ba7d6dc09
German Beldorati Stark <gerbeldo@gmail.com>
2022-10-10 19:00:08 -0300

    actually move it

--- pipeline-runner/init.R
+++ pipeline-runner/init.R
@@ -4,9 +4,6 @@ library(zeallot)
 library(tryCatchLog)
 library(magrittr)
 
-# time stamp used for directory to store log/dump files in event of error
-debug_timestamp <- format(Sys.time(), format = "%Y-%m-%d_at_%H-%M-%OS3")
-
 # increase maxSize from the default of 500MB to 32GB
 options(future.globals.maxSize = 32 * 1024 * 1024^2)
 
