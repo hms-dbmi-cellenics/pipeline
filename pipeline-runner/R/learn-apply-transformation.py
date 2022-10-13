a244ec1a08dc2d71511d4893e42aa97625b62659
sara castellano <sara@biomage.net>
2022-10-13 11:26:22 +0200

    move initialisation of X_int lower

--- pipeline-runner/R/learn-apply-transformation.py
+++ pipeline-runner/R/learn-apply-transformation.py
@@ -4,8 +4,6 @@ from sklearn.neighbors import NearestNeighbors
 # Apply integrated coordinates back to full data.
 def apply_transf(unsketched_data, sketched_data, sketched_integrated_data):
     for i, (X_unsketch, X_sketch) in enumerate(zip(unsketched_data, sketched_data)):
-        X_int = sketched_integrated_data[i]
-
         neigh = NearestNeighbors(n_neighbors=3).fit(X_unsketch)
         _, neigh_idx = neigh.kneighbors(X_sketch)
 
@@ -14,7 +12,8 @@ def apply_transf(unsketched_data, sketched_data, sketched_integrated_data):
             for k_idx in range(neigh_idx.shape[1]):
                 ds_idxs.append(neigh_idx[ref_idx, k_idx])
                 ref_idxs.append(ref_idx)
-
+                
+        X_int = sketched_integrated_data[i]
         bias = transform(X_unsketch, X_int, ds_idxs, ref_idxs, 15, batch_size=1000)
 
         sketched_integrated_data[i] = X_unsketch + bias
