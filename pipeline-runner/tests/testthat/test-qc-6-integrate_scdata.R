eebed6b38ff161dd591b992b0768c7f2552ba5c7
sara castellano <sara@biomage.net>
2022-10-18 15:27:15 +0200

    fix variable name in test

--- pipeline-runner/tests/testthat/test-qc-6-integrate_scdata.R
+++ pipeline-runner/tests/testthat/test-qc-6-integrate_scdata.R
@@ -480,7 +480,7 @@ test_that("run_geosketch generates the correct number of sketches", {
   perc_num_cells <- 5
   num_cells <- round(ncol(merged_scdata) * perc_num_cells / 100)
   c(scdata, scdata_sketch) %<-% run_geosketch(merged_scdata, dims = 50, perc_num_cells)
-  expect_equal(ncol(sketched_data), num_cells)
+  expect_equal(ncol(scdata_sketch), num_cells)
 })
 
 
