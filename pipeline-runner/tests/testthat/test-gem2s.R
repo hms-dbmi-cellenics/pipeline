73e968054de8edfe54211fbf8ac63a56a8b9e7a3
German Beldorati Stark <gerbeldo@gmail.com>
2022-11-01 11:41:03 -0300

    remove old test version, add docs

--- pipeline-runner/tests/testthat/test-gem2s.R
+++ pipeline-runner/tests/testthat/test-gem2s.R
@@ -28,96 +28,6 @@ gem2s_setup <- function(experiment_id) {
 }
 
 
-#' Run gem2s sequentially and snapshot test each step
-#'
-#' The snapshots saved are only the structure of each steps output, except for
-#' the final object (the output to prepare experiment) which is completely saved
-#' as a text representation (using `dump`). That can be sourced and used as input
-#' for similar tests in QC.
-#'
-#' To run on other experiments two things are required:
-#'
-#' 1. add sample files to the `mock_data/originals_bucket` folder
-#' 2. create an `input.json` file, named like `<experiment_id>-input.json` and put it
-#'    in the `mock_data/input` folder. It should contain the correct sample file
-#'    ids (the name of the sample files in the `originals_bucket` folder) and
-#'    `experiment_id`
-#'
-#' @param experiment_id character
-#'
-test_gem2s <- function(experiment_id) {
-  test_that("some gem2s steps work", {
-    setup <- gem2s_setup(experiment_id)
-
-    # gem2s 1
-
-    res <- stubbed_download_user_files(setup$input, setup$pipeline_config)
-    expect_snapshot(str(res, vec.len = 20))
-
-    # gem2s 2
-
-    # folder where download_user_files puts things in
-    input_path <- file.path(setup$paths$base, "input")
-    res <- load_user_files(setup$input, NULL, res$output, input_path)
-    expect_snapshot(str(res, vec.len = 20))
-
-    # gem2s 3
-
-    res <- run_emptydrops(setup$input, NULL, res$output)
-    expect_snapshot(str(res, vec.len = 20))
-
-    # gem2s 4
-
-    res <- score_doublets(setup$input, NULL, res$output)
-    expect_snapshot(str(res, vec.len = 20))
-
-    # gem2s 5
-
-    res <- create_seurat(setup$input, NULL, res$output)
-    expect_snapshot(str(res, vec.len = 20))
-
-    # gem2s 6
-    step_n <- 6
-    res <- prepare_experiment(setup$input, NULL, res$output)
-    expect_snapshot(str(res, vec.len = 20))
-
-    # snapshot qc_config independently as it is used frequently in QC tests
-    qc_config <- res$output$qc_config
-    qc_config_snapshot_name <- make_snapshot_name(step_n, experiment_id, "qc_config.R")
-    withr::with_tempfile("tf_qc_config", {
-      dump("qc_config", tf_qc_config)
-      expect_snapshot_file(tf_qc_config, name = qc_config_snapshot_name)
-    })
-
-    # fully snapshot final gem2s file (step 7 does not return the scdata_list)
-    snapshot_name <- make_snapshot_name(step_n, experiment_id, "out.R")
-    withr::with_tempfile("tf", {
-      dump("res", tf)
-      expect_snapshot_file(tf, name = snapshot_name)
-    })
-
-
-    #gem2s 7
-    step_n <- 7
-
-    res <- stubbed_upload_to_aws(setup$input, setup$pipeline_config, res$output)
-    expect_snapshot(str(res, vec.len = 20))
-
-    # cellsets file
-    cellset_snapshot_name <- make_snapshot_name(step_n, experiment_id, "cellsets.json")
-    expect_snapshot_file(
-      file.path(setup$pipeline_config$cell_sets_bucket, experiment_id),
-      name = cellset_snapshot_name
-    )
-
-    # cleanup
-    withr::defer(unlink(setup$pipeline_config$cell_sets_bucket, recursive = TRUE))
-    withr::defer(unlink(setup$pipeline_config$source_bucket, recursive = TRUE))
-    withr::defer(unlink(file.path(setup$paths$mock_data, "temp"), recursive = TRUE))
-  })
-
-}
-
 snapshot_final_output <- function(res, experiment_id){
 
   qc_config <- res$output$qc_config
@@ -137,10 +47,26 @@ snapshot_final_output <- function(res, experiment_id){
 
 }
 
-#test_gem2s("mock_experiment_id")
 
-test_gem2s_v3 <- function(experiment_id) {
-  test_that("gem2s v3", {
+#' Run gem2s sequentially and snapshot test each step
+#'
+#' The snapshots saved are only the structure of each steps' output and the hash,
+#' except for the final object (the output to prepare experiment) which is
+#' completely saved as a text representation (using `dump`). That can be sourced
+#' and used as input for similar tests in QC.
+#'
+#' To run on other experiments two things are required:
+#'
+#' 1. add sample files to the `mock_data/originals_bucket` folder
+#' 2. create an `input.json` file, named like `<experiment_id>-input.json` and put it
+#'    in the `mock_data/input` folder. It should contain the correct sample file
+#'    ids (the name of the sample files in the `originals_bucket` folder) and
+#'    `experiment_id`
+#'
+#' @param experiment_id character
+#'
+test_gem2s <- function(experiment_id) {
+  test_that("gem2s is reproducible", {
 
     paths <- setup_test_paths()
     input <- load_experiment_input(paths$mock_data, experiment_id)
@@ -183,4 +109,4 @@ test_gem2s_v3 <- function(experiment_id) {
 
 }
 
-test_gem2s_v3("mock_experiment_id")
+test_gem2s("mock_experiment_id")
