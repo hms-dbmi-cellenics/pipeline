67a4311da6dac50f6cf39ee9627b592ac8774889
German Beldorati Stark <gerbeldo@gmail.com>
2022-12-23 13:54:50 -0300

    comment WIP tests

--- pipeline-runner/tests/testthat/test-subset-1-subset_seurat.R
+++ pipeline-runner/tests/testthat/test-subset-1-subset_seurat.R
@@ -1,25 +1,39 @@
-mock_scdata <- function(){
-  processed_path <- "/Users/german/bm/cellenics/data/8ecc9d20-30e4-49eb-b536-a0d1f0ba420d/processed_r.rds"
-  readRDS(processed_path)
-}
+# mock_scdata <- function(){
+#   paths <- path_setup()
+#   source("tests/testthat/_snaps/qc/mock_experiment_id-integrated_scdata.R")
+#   scdata <- snap_list$data
+#   rm(snap_list, envir = parent.frame())
+#   return(scdata)
+# }
+#
+# mock_cellsets <- function(){
+#
+#   jsonlite::fromJSON("tests/testthat/_snaps/gem2s/gem2s-7-mock_experiment_id-cellsets.json", flatten = TRUE)
+#
+# }
+#
+# mock_cluster_cellsets <- function(cellsets) {
+#
+# }
+#
+# mock_input <- function() {
+#   input <- list(
+#     name = "mock_subset_experiment_name",
+#     parentExperimentId = "mock_parent_experiment_id",
+#     subsetExperimentId = "mock_subset_experiment_id",
+#     cellSetKeys =  c("louvain-0", "louvain-1")
+#   )
+#
+#   return(input)
+# }
+#
+# parent_scdata <- mock_scdata()
+# cellsets <- mock_cellsets()
+# parent_cellsets <- parse_cellsets(mock_cellsets())
+#
+# parent <- list(scdata = parent_scdata, cellsets = parent_cellsets)
+#
+# input <- mock_input()
 
-mock_cellsets <- function(){
-  cellsets_path <- "/Users/german/bm/cellenics/data/8ecc9d20-30e4-49eb-b536-a0d1f0ba420d/cellsets.json"
-  jsonlite::fromJSON(cellsets_path, flatten = TRUE)
-}
 
-mock_input <- function() {
-  input <- list(
-    name = "mock_subset_experiment_name",
-    parentExperimentId = "mock_parent_experiment_id",
-    subsetExperimentId = "mock_subset_experiment_id",
-    cellSetKeys =  c("louvain-0", "louvain-1")
-  )
 
-  return(input)
-}
-
-parent_scdata <- mock_scdata()
-parent_cellsets <- parse_cellsets(mock_cellsets())
-sample_mapping <- mock_sample_id_mapping()
-input <- mock_input()
