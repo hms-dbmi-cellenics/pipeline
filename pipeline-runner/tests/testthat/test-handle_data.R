dc1e6113b607f5130dbb33a72d2f8f950ce17896
German Beldorati Stark <gerbeldo@gmail.com>
2023-03-17 17:18:56 -0300

    add tests for helpers

--- pipeline-runner/tests/testthat/test-handle_data.R
+++ pipeline-runner/tests/testthat/test-handle_data.R
@@ -171,3 +171,44 @@ test_that("parse_cellsets parses a cellset object", {
   expect_identical(names(res), c("key", "name", "type", "cell_id"))
 
 })
+
+
+test_that("is_uuid detects uuids correctly", {
+
+  expect_true(is_uuid(uuid::UUIDgenerate()))
+  expect_false(is_uuid("not-a-uuid"))
+
+})
+
+
+test_that("get_cellset_types correctly gets cellset types", {
+  key <-
+    c(
+      "louvain",
+      "scratchpad",
+      "sample",
+      "metadata_1",
+      "metadata_2",
+      "faa74528-c4d7-11ed-9fda-0242ac120003"
+    )
+
+  type <-
+    c(
+      "cellSets",
+      "cellSets",
+      "metadataCategorical",
+      "metadataCategorical",
+      "metadataCategorical",
+      "cellSets"
+    )
+
+  expected_cellset_types <- c("cluster",
+                              "scratchpad",
+                              "sample",
+                              "metadata",
+                              "metadata",
+                              "sctype")
+
+  expect_identical(purrr::map2_chr(key, type, get_cellset_type),
+                   expected_cellset_types)
+})
