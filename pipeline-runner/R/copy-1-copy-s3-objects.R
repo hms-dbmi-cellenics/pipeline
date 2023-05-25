1b1d9a0de3b7299f9ae8f189637051eec944ea6a
cosa65 <martin@biomage.net>
2023-05-25 09:48:37 -0300

    Add functionality for copy to translate proccesed rds and cell sets

--- pipeline-runner/R/copy-1-copy-s3-objects.R
+++ pipeline-runner/R/copy-1-copy-s3-objects.R
@@ -1,64 +1,31 @@
-# #' create a subset experiment
-# #'
-# #' This is the first step of a subset pipeline, which basically takes the parent
-# #' experiment ID and cellset keys to keep as input, extracts the cell ids to keep
-# #' and subsets and slims down the parent seurat object.
-# #'
-# #' @param input list containing:
-# #'   - parentExperimentId character
-# #'   - subsetExperimentId character
-# #'   - cellSetKeys character vector of cellset keys to subset
-# #'   - experimentName character
-# #'   - parentProcessingConfig The processingConfig of the parent experiment
-# #' @param pipeline_config list
-# #' @param prev_out list, ignored because this is the first step in the subset pipeline
-# #'
-# #' @return list containing scdata_list, annotations and sample_id_map
-# #' @export
-# #'
-# subset_seurat <- function(input, pipeline_config, prev_out = NULL) {
-#   parent_data <- load_parent_experiment_data(input, pipeline_config)
-
-#   subset_scdata <- subset_experiment(input, parent_data)
-#   sample_id_map <- create_sample_id_map(unique(subset_scdata$samples))
-#   subset_scdata <- add_subset_metadata(input, subset_scdata, sample_id_map)
-
-#   subset_scdata_list <- Seurat::SplitObject(subset_scdata, split.by = "samples")
-
-#   # TODO: remove from here and refactor all pipeline.
-#   config <- list(
-#     name = input$experimentName,
-#     samples = sample_id_map$subset_sample_id
-#   )
-
-#   # structure step output
-#   res <- list(
-#     data = list(
-#       sampleIdMap = sample_id_map
-#       ),
-#     output = list(
-#       scdata_list = subset_scdata_list,
-#       annot = subset_scdata@misc$gene_annotations,
-#       edrops = NULL,
-#       sample_id_map = sample_id_map,
-#       config = config,
-#       disable_qc_filters = TRUE,
-#       parent_cellsets = parent_data$cellsets,
-#       qc_config = generate_subset_config(input$parentProcessingConfig, sample_id_map)
-#     )
-#   )
-
-#   message("\nSubsetting of Seurat object step complete.")
-#   return(res)
-# }
-
 copy_s3_objects <- function(input, pipeline_config, prev_out = NULL) {
-  parent_data <- load_from_experiment_data(input, pipeline_config)
+  from_experiment_id <- input$fromExperimentId
+  to_experiment_id <- input$toExperimentId
 
-  parent_scdata <- parent_data$parent_scdata
-  parent_cellsets <- parent_data$parent_cellsets
+  original_data <- load_from_experiment_data(from_experiment_id, pipeline_config)
 
-  
+  original_scdata <- original_data$scdata
+  original_cell_sets <- original_data$cell_sets
+  sample_ids_map <- input$sampleIdsMap
+
+  translated_scdata <- translate_sample_ids(original_scdata, sample_ids_map)
+  translated_scdata@misc$experimentId <- to_experiment_id
+
+  translated_cell_sets <- list()
+  translated_cell_sets$cellSets <- format_cell_sets(
+    translate_cell_sets(original_cell_sets$cellSets, sample_ids_map)
+  )
+
+  # cell sets file to s3
+  put_object_in_s3(pipeline_config,
+    bucket = pipeline_config$cell_sets_bucket,
+    object = charToRaw(RJSONIO::toJSON(translated_cell_sets)),
+    key = to_experiment_id
+  )
+
+  upload_matrix_to_s3(pipeline_config, to_experiment_id, translated_scdata)
+
+  return()
 }
 
 
@@ -72,25 +39,14 @@ copy_s3_objects <- function(input, pipeline_config, prev_out = NULL) {
 #' @return list with scdata and parsed cellsets
 #' @export
 #'
-load_from_experiment_data <- function(input, pipeline_config) {
-
-  str("inputDebug")
-  str(input)
-
-  message("inputDebug")
-  message(input)
-
+load_from_experiment_data <- function(experiment_id, pipeline_config) {
   # load parent processed scdata and cellsets
   s3 <- paws::s3(config = pipeline_config$aws_config)
-  parent_scdata <-
-    load_processed_scdata(s3, pipeline_config, input$fromExperimentId)
-  parent_cellsets <-
-    parse_cellsets(load_cellsets(s3, pipeline_config, input$fromExperimentId))
+  parent_scdata <- load_processed_scdata(s3, pipeline_config, experiment_id)
+  # parent_cellsets <- parse_cellsets(load_cellsets(s3, pipeline_config, experiment_id))
+  parent_cellsets <- load_cellsets(s3, pipeline_config, experiment_id)
 
-  str("inputsampleIdsMapDebug")
-  str(input$sampleIdsMap)
-
-  return(list(scdata = parent_scdata, cellsets = parent_cellsets))
+  return(list(scdata = parent_scdata, cell_sets = parent_cellsets))
 }
 
 #' Add new sample ids to the subset Seurat Object
@@ -101,10 +57,64 @@ load_from_experiment_data <- function(input, pipeline_config) {
 #' @return SeuratObject with new sample ids
 #' @export
 #'
-add_new_sample_ids <- function(scdata, sample_id_map) {
-  sample_map_idx <- match(subset_scdata$samples, names(sample_id_map))
-  str("sample_map_idxDebug")
-  str(sample_map_idx)
-  subset_scdata$samples <- unname(unlist(sample_id_map[sample_map_idx]))
-  return(subset_scdata)
+translate_sample_ids <- function(scdata, sample_id_map) {
+  sample_map_idx <- match(scdata$samples, names(sample_id_map))
+  scdata$samples <- unname(unlist(sample_id_map[sample_map_idx]))
+
+  return(scdata)
+}
+
+translate_cell_sets <- function(cell_sets, sample_ids_map) {
+  # Loop through each element in the 'children' list
+  for (i in seq_along(cell_sets$children)) {
+    # Get the 'key' value
+    key <- cell_sets$children[[i]]$key
+
+    # Apply translate() to the values in the list that exist in the vector
+    cell_sets$children[[i]]$key <- lapply(
+      key,
+      function(current_key) {
+        ifelse(
+          current_key %in% names(sample_ids_map),
+          sample_ids_map[[current_key]],
+          current_key
+        )
+      }
+    )
+  }
+
+  return(cell_sets)
+}
+
+format_cell_sets <- function(cell_sets) {
+  formatted <- list()
+
+  for (i in seq_along(cell_sets$key)) {
+    cell_class <- list(
+      key = cell_sets$key[[i]],
+      name = cell_sets$name[[i]],
+      rootNode = cell_sets$rootNode[[i]],
+      type = cell_sets$type[[i]],
+      children = list()
+    )
+
+    for (j in seq_along(cell_sets$children[[i]]$key)) {
+      children <- cell_sets$children[[i]]
+
+      cell_set <- list(
+        key = children$key[[j]],
+        name = children$name[[j]],
+        rootNode = children$rootNode[[j]],
+        color = children$color[[j]],
+        type = children$type[[j]],
+        cellIds = ensure_is_list_in_json(children$cellIds[[j]])
+      )
+
+      cell_class$children <- append(cell_class$children, list(cell_set))
+    }
+
+    formatted <- append(formatted, list(cell_class))
+  }
+
+  return(formatted)
 }
