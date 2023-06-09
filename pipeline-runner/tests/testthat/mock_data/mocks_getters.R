2526ac049388bbe12e5135859018270a81da062f
cosa65 <martin@biomage.net>
2023-06-09 11:50:18 -0300

    Try adding source

--- pipeline-runner/tests/testthat/mock_data/mocks_getters.R
+++ pipeline-runner/tests/testthat/mock_data/mocks_getters.R
@@ -1,3 +1,5 @@
+source("../helper-stub_functions")
+
 get_mock_cell_sets <- function(flatten = TRUE) {
   cell_sets_path <- file.path(setup_test_paths()$mock_data, "cell_sets")
   path <- file.path(cell_sets_path, "cell_sets_2_samples.json")
