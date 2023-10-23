743a1012c1f42535ac0fd20719d009143c238e8a
German Beldorati Stark <gerbeldo@gmail.com>
2023-10-23 09:45:25 -0400

    fix: gtools is failing in image
    Signed-off-by: German Beldorati Stark <gerbeldo@gmail.com>

--- pipeline-runner/R/qc-7-embed_and_cluster.R
+++ pipeline-runner/R/qc-7-embed_and_cluster.R
@@ -88,7 +88,7 @@ format_cell_sets_object <- function(
         type = "cellSets",
         children = list()
       )
-    for (cluster in gtools::mixedsort(unique(cell_sets$cluster))) {
+    for (cluster in stringr::str_sort(unique(cell_sets$cluster), numeric = TRUE)) {
       cells <- cell_sets[cell_sets$cluster == cluster, "cell_ids"]
       is.num <- !is.na(as.numeric(cluster))
       set_name <- ifelse(is.num, paste("Cluster", cluster), cluster)
