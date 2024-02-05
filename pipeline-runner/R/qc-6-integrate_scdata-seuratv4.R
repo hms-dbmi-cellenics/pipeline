aed75d4fc2b46eb7bebf35dcba01c1ec16eccc93
Sara Castellano <37797231+saracastel@users.noreply.github.com>
2024-02-05 13:25:28 +0100

    Replace switch with if/else
    Co-authored-by: German Beldorati Stark <gerbeldo@gmail.com>

--- pipeline-runner/R/qc-6-integrate_scdata-seuratv4.R
+++ pipeline-runner/R/qc-6-integrate_scdata-seuratv4.R
@@ -231,16 +231,12 @@ seuratv4_geosketch_find_and_integrate_anchors <-
     # merge
     scdata <- create_scdata(scdata_list, cells_id, merge_data = TRUE)
     # geosketch needs PCA to be run
-    active_assay <- Seurat::DefaultAssay(scdata)
-    switch(active_assay,
-      RNA = {
-        scdata <- Seurat::FindVariableFeatures(scdata, assay = "RNA", nfeatures = 2000, verbose = FALSE)
-      },
-      SCT = {
-        scdata_features <- Seurat::SelectIntegrationFeatures(object.list = scdata_list, nfeatures = 2000)
-        Seurat::VariableFeatures(scdata[["SCT"]]) <- scdata_features
-      }
-    )
+  if (Seurat::DefaultAssay(scdata) == "SCT") {
+    scdata_features <- Seurat::SelectIntegrationFeatures(object.list = scdata_list, nfeatures = 2000)
+    Seurat::VariableFeatures(scdata[["SCT"]]) <- scdata_features
+  } else {
+    scdata <- Seurat::FindVariableFeatures(scdata, assay = "RNA", nfeatures = 2000, verbose = FALSE)
+  }
     scdata <- scdata |>
       Seurat::ScaleData(verbose = FALSE) |>
       Seurat::RunPCA(npcs = npcs, verbose = FALSE)
