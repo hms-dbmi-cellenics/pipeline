9c4e8646f6452a624cefcdb681e1a12aac5b116a
Alex Pickering <alexvpickering@gmail.com>
2024-05-22 13:54:20 -0700

    move helpers to seperate file
    Signed-off-by: Alex Pickering <alexvpickering@gmail.com>

--- pipeline-runner/tests/testthat/test-qc-6-integrate_scdata-seuratv4.R
+++ pipeline-runner/tests/testthat/test-qc-6-integrate_scdata-seuratv4.R
@@ -104,48 +104,6 @@ mock_doublet_scores <- function(counts) {
   )
 }
 
-
-#' Fix time stamps in seurat object
-#'
-#' Seurat adds logs to certain command runs, with time stamps that make snapshot
-#' tests fail. This function replaces them with a fixed datetime object.
-#'
-#' @param scdata seurat object
-#'
-#' @return seurat object with fixed time stamps
-#'
-clean_timestamp <- function(scdata) {
-  fixed_datetime <- as.POSIXct("1991-12-19 05:23:00", tz = "UTC")
-
-  for (slot in names(scdata@commands)) {
-    scdata@commands[[slot]]@time.stamp <- fixed_datetime
-  }
-
-  if ("ingestionDate" %in% names(scdata@misc)) {
-    scdata@misc$ingestionDate <- fixed_datetime
-  }
-
-  return(scdata)
-}
-
-# Seurat object commands are sometimes functions which will have a new environment
-# for every call which breaks snapshots
-remove_commands_functions <- function(data) {
-  command_names <-  names(data@commands)
-  for (command_name in command_names) {
-    param_names <- names(data@commands[[command_name]]@params)
-
-    for (param_name in param_names) {
-      param <- data@commands[[command_name]]@params[[param_name]]
-
-      if (methods::is(param, 'function'))
-        data@commands[[command_name]]@params[[param_name]] <- NULL
-    }
-  }
-  return(data)
-}
-
-
 test_that("SeuratV4 integration doesnt error out with small dataset", {
   c(scdata_list, sample_1_id, sample_2_id) %<-% mock_scdata()
   cells_id <- mock_ids()
--- pipeline-runner/tests/testthat/test-qc.R
+++ pipeline-runner/tests/testthat/test-qc.R
@@ -26,48 +26,6 @@ qc_setup <- function(experiment_id) {
   ))
 }
 
-
-#' Fix time stamps in seurat object
-#'
-#' Seurat adds logs to certain command runs, with time stamps that make snapshot
-#' tests fail. This function replaces them with a fixed datetime object.
-#'
-#' @param scdata seurat object
-#'
-#' @return seurat object with fixed time stamps
-#'
-clean_timestamp <- function(scdata) {
-  fixed_datetime <- as.POSIXct("1991-12-19 05:23:00", tz = "UTC")
-
-  for (slot in names(scdata@commands)) {
-    scdata@commands[[slot]]@time.stamp <- fixed_datetime
-  }
-
-  if ("ingestionDate" %in% names(scdata@misc)) {
-    scdata@misc$ingestionDate <- fixed_datetime
-  }
-
-  return(scdata)
-}
-
-# Seurat object commands are sometimes functions which will have a new environment
-# for every call which breaks snapshots
-remove_commands_functions <- function(data) {
-  command_names <-  names(data@commands)
-  for (command_name in command_names) {
-    param_names <- names(data@commands[[command_name]]@params)
-
-    for (param_name in param_names) {
-      param <- data@commands[[command_name]]@params[[param_name]]
-
-      if (methods::is(param, 'function'))
-        data@commands[[command_name]]@params[[param_name]] <- NULL
-    }
-  }
-  return(data)
-}
-
-
 snapshot_qc_output <- function(task_name, snap_list) {
   # scdata_list is returned for every sample, so we'll have duplicated stuff here.
   if (is.list(snap_list$data)) {
