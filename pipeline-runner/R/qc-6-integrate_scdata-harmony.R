6ef9d4a7b1fecb6b2abc61fe73d64db14051dc90
Alex Pickering <alexvpickering@gmail.com>
2026-03-19 05:24:27 -0400

    use seurat builtins for geosketch and harmony; factor out pre-processing
    Signed-off-by: Alex Pickering <alexvpickering@gmail.com>

--- pipeline-runner/R/qc-6-integrate_scdata-harmony.R
+++ pipeline-runner/R/qc-6-integrate_scdata-harmony.R
@@ -2,6 +2,7 @@ run_harmony <- function(scdata_list, config, cells_id) {
   settings <- config$dataIntegration$methodSettings[["harmony"]]
   nfeatures <- settings$numGenes
   normalization <- settings$normalisation
+
   if (grepl("lognorm", normalization, ignore.case = TRUE)) {
     normalization <- "LogNormalize"
   }
@@ -15,8 +16,6 @@ run_harmony <- function(scdata_list, config, cells_id) {
     npcs <- min(config$dimensionalityReduction$numPCs, npcs_for_pca)
   }
 
-  use_geosketch <- "downsampling" %in% names(config) && config$downsampling$method == "geosketch"
-
   scdata <- prepare_scdata_for_harmony(scdata_list, config, cells_id)
 
   # we need RNA assay to compute the integrated matrix
@@ -25,68 +24,158 @@ run_harmony <- function(scdata_list, config, cells_id) {
   # required pre-processing
   # run PCA with npcs_for_pca for the elbow plot and the % of variance explained
   scdata <- scdata |>
-    Seurat::NormalizeData(normalization.method = normalization, verbose = FALSE) |>
-    Seurat::FindVariableFeatures(nfeatures = nfeatures, verbose = FALSE) |>
-    Seurat::ScaleData(verbose = FALSE) |>
-    run_pca(npcs = npcs_for_pca, verbose = FALSE)
+    RunPreprocessing(
+      normalization = normalization,
+      nfeatures = nfeatures,
+      npcs = npcs_for_pca,
+      config = config
+    )
 
+  use_geosketch <- is_geosketch(config)
+  pca.reduction <- ifelse(use_geosketch, "pca.sketch", "pca")
+  new.reduction <- ifelse(use_geosketch, "harmony.sketch", "harmony")
 
   # estimate number of PCs to be used downstream, for integration and clustering
   if (is.null(npcs)) {
-    scdata@misc[["active.reduction"]] <- "pca"
+    scdata@misc[["active.reduction"]] <- pca.reduction
     npcs <- get_npcs(scdata)
     message("Estimated number of PCs: ", npcs)
   }
 
-  # downsample
-  # main function
+  scdata <- Seurat::IntegrateLayers(
+    scdata,
+    method = Seurat::HarmonyIntegration,
+    orig = pca.reduction,
+    new.reduction = new.reduction,
+    dims = 1:npcs,
+    verbose = FALSE
+  )
+
   if (use_geosketch) {
-    scdata <-
-      RunGeosketchHarmony(
-        scdata,
-        group.by.vars = "samples",
-        npcs = npcs,
-        config
-      )
-  } else {
-    scdata <-
-      harmony::RunHarmony(
-        scdata,
-        group.by.vars = "samples",
-        dims.use = 1:npcs
-      )
+    scdata <- ProjectGeosketchIntegration(
+      scdata,
+      npcs,
+      sketched.reduction = "harmony.sketch",
+      full.reduction = "harmony"
+    )
   }
 
   scdata <- add_dispersions(scdata, normalization)
   scdata@misc[["numPCs"]] <- npcs
-  scdata@misc[["active.reduction"]] <- "harmony"
+  scdata@misc[["active.reduction"]] <- new.reduction
 
   return(scdata)
 }
 
-run_pca <- function(scdata, npcs) {
-  scale.data <- scdata[['RNA']]$scale.data
-  scale.data_dir <- file.path(tempdir(), 'scale.data_bpcells')
+ProjectGeosketchIntegration <- function(scdata, npcs, sketched.reduction, full.reduction, is.unisample = FALSE) {
+
+  if (!is.unisample) {
+    # project full data onto integrated DR from sketch
+    scdata <- Seurat::ProjectIntegration(
+      object = scdata,
+      assay = "RNA",
+      sketched.assay = "sketch",
+      reduction = sketched.reduction,
+      reduction.name = full.reduction
+    )
+  }
+
+  # for unisample: projects full PCA
+  # for all: called to pre-compute weights and neighbors (stored in tools)
+  # for future projections (clustering and UMAP)
+  message("Projecting data onto integrated space...")
+  scdata <- Seurat::ProjectData(
+    object = scdata,
+    assay = "RNA",
+    sketched.assay = "sketch",
+    full.reduction = full.reduction,
+    sketched.reduction = sketched.reduction,
+    dims = 1:npcs
+  )
+
+  # uploaded after integration and before clustering
+  # worker assumes default assay is RNA
+  Seurat::DefaultAssay(scdata) <- "RNA"
+  return(scdata)
+}
+
+is_geosketch <- function(config) {
+  "downsampling" %in% names(config) && config$downsampling$method == "geosketch"
+}
+
+RunPreprocessing <- function(scdata, normalization, nfeatures, npcs, config) {
+  
+  # SketchData takes normalized data and set of variable features
+  scdata <-
+    Seurat::NormalizeData(
+      scdata,
+      normalization.method = normalization,
+      verbose = FALSE
+    )
+
+  # split assay into sample layers
+  num_samples <- length(unique(scdata$samples))
+  if (num_samples > 1) {
+    scdata[["RNA"]] <- split(scdata[["RNA"]], f = scdata$samples)
+  }
+
+  scdata <- Seurat::FindVariableFeatures(scdata, nfeatures = nfeatures, verbose = FALSE)
+
+  message("Is this GEOSKETCH? ", is_geosketch(config))
+
+  if (is_geosketch(config)) {
+    perc_num_cells <- config$downsampling$methodSettings$geosketch$percentageToKeep
+    num_cells <- round(ncol(scdata) * (perc_num_cells / 100) / num_samples)
+    message("Sketching data with Geosketch, keeping ", num_cells, " cells per sample.")
+    scdata <- Seurat::SketchData(
+      object = scdata,
+      ncells = num_cells,
+      method = "LeverageScore",
+      sketched.assay = "sketch"
+    )
+  
+    # vignettes run FindVariableFeatures before and after SketchData
+    Seurat::DefaultAssay(scdata) <- "sketch"
+    scdata <- Seurat::FindVariableFeatures(scdata, nfeatures = nfeatures, verbose = FALSE)
+  }
+
+  reduction.name <- ifelse(is_geosketch(config), "pca.sketch", "pca")
+
+  scdata <- scdata |>
+    Seurat::ScaleData(verbose = FALSE) |>
+    RunPCA(npcs = npcs, reduction.name = reduction.name)
+
+  return(scdata)
+}
+
+RunPCA <- function(scdata, npcs, reduction.name = "pca") {
 
   # if BPCells write out operations for faster PCA
-  if (is(scale.data, "IterableMatrix")) {
+  is.bpcells <- is(scdata[['RNA']]$scale.data, "IterableMatrix")
 
-    BPCells::write_matrix_dir(scale.data, scale.data_dir)
-    scdata[['RNA']]$scale.data <- BPCells::open_matrix_dir(scale.data_dir)
+  if (is.bpcells) {
+    data_dir <- tempfile()
+    scale.data <- scdata[['RNA']]$scale.data
+    disk.scale.data <- BPCells::write_matrix_dir(scale.data, data_dir)
+    scdata[['RNA']]$scale.data <- disk.scale.data
   }
 
   # run PCA
+  message('Running PCA ...')
   scdata <- Seurat::RunPCA(
     scdata,
-    npcs = npcs_for_pca,
+    npcs = npcs,
+    reduction.name = reduction.name,
     verbose = FALSE
   )
+  message('PCA complete.')
 
-  # cleanup disk scale.data
-  unlink(scale.data_dir, recursive = TRUE)
+  if (is.bpcells) {
+    # cleanup restore scale.data to original object (removed disk backing)
+    unlink(data_dir, recursive = TRUE)
+    scdata[['RNA']]$scale.data <- scale.data
+  }
 
-  # restore scale.data to BPCells operations
-  scdata[['RNA']]$scale.data <- scale.data
   return(scdata)
 }
 
